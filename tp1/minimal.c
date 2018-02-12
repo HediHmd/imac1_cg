3ec33c0ea0ff67726d81eb6443aa2620a4bce3c7
HAMADACHE HEDI <hhamadac@localhost.localdomain>
2018-02-12 10:30:17 +0100

    ex6 - fonctions

--- tp1/minimal.c
+++ tp1/minimal.c
@@ -14,7 +14,78 @@ static const unsigned int BIT_PER_PIXEL = 32;
 /* Nombre minimal de millisecondes separant le rendu de deux images */
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
 
-void dessin(int WINDOW_WIDTH, int WINDOW_HEIGHT){
+/* Struct */
+
+typedef struct Point{
+    float x, y; //position 2D du point
+    unsigned char r, g, b; // Couleur du point
+    struct Point* next; // Point suivant à dessiner
+} Point, *PointList;
+
+typedef struct Primitive{
+    GLenum primitiveType;
+    PointList points;
+    struct Primitive* next;
+} Primitive, *PrimitiveList;
+
+/* Points */
+
+Point* allocPoint(float x, float y, unsigned char r, unsigned char g, unsigned char b){
+    Point* point = (Point*) malloc(sizeof(point));
+    if(!point) {
+        return NULL;
+    }
+    point->x = x;
+    point->y = y;
+    point->r = r;
+    point->g = g;
+    point->b = b;
+    point->next = NULL;
+    return point;
+}
+
+void addPointToList(Point* point, PointList* list){
+    if(*list == NULL) {
+        *list = point;
+    }
+
+    else {
+    addPointToList(point, &(*list)->next);
+    }
+}
+
+void drawPoints(PointList list){
+    while(list!=NULL){
+        glColor3ub(list->r, list->g, list->b);
+        glVertex2f(list->x, list->y);
+        list=list->next;
+    }
+}
+
+void deletePoints(PointList* list){
+PointList* tmp;
+    while(list!=NULL){
+        tmp = &(*list)->next; 
+        free(*list);
+        list = tmp;
+    }
+}
+
+
+/* Primitives */
+
+Primitive* allocPrimitive(GLenum primitiveType);
+
+void addPrimitive(Primitive* primitive, PrimitiveList* list);
+
+void drawPrimitives(PrimitiveList list);
+
+void deletePrimitive(PrimitiveList* list);
+
+/* Resize */
+
+void resize(int WINDOW_WIDTH, int WINDOW_HEIGHT){
+    SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_RESIZABLE);
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -29,8 +100,10 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
     
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 0);
+
     /* Ouverture d'une fenêtre et création d'un contexte OpenGL */
-    if(NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE)) {
+    if(NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_RESIZABLE)) {
         fprintf(stderr, "Impossible d'ouvrir la fenetre. Fin du programme.\n");
         return EXIT_FAILURE;
     }
@@ -38,19 +111,15 @@ int main(int argc, char** argv) {
     /* Titre de la fenêtre */
     SDL_WM_SetCaption("OpenGL de Hédi", NULL);
 
+    SDL_GL_SwapBuffers();
+    glClear(GL_COLOR_BUFFER_BIT);
+
     /* Boucle d'affichage */
     int loop = 1;
     while(loop) {
 
         /* Récupération du temps au début de la boucle */
         Uint32 startTime = SDL_GetTicks();
-        
-        /* Placer ici le code de dessin */
-        
-        /* Echange du front et du back buffer : mise à jour de la fenêtre */
-        SDL_GL_SwapBuffers();
-        glClear(GL_COLOR_BUFFER_BIT);
-
 
         /* Boucle traitant les evenements */
         SDL_Event e;
@@ -65,23 +134,23 @@ int main(int argc, char** argv) {
 
                 /* Clic souris */
                 case SDL_MOUSEBUTTONUP:
-                    printf("clic en (%d, %d)\n", e.button.x, e.button.y);
-                    float r, v, b;
-                    r = e.button.x / (float)WINDOW_WIDTH;
-                    v = e.button.y / (float)WINDOW_HEIGHT;
-                    b = 0;
-                    glClearColor(r, v, b, 1);
+                        glBegin(GL_POINTS);
+                        glColor3ub(0, 255, 0);
+                        glVertex2f(-1 + 2. * e.button.x / WINDOW_WIDTH, - (-1 + 2. * e.button.y / WINDOW_HEIGHT));
+                        glEnd();
+                        SDL_GL_SwapBuffers();
                     break;
 
                 /* Touche clavier */
                 case SDL_KEYDOWN:
-                    if (e.key.keysym.sym == 113){
+                    if (e.key.keysym.sym == 112){
                         return 0; 
                     }
                     printf("touche pressée (code = %d)\n", e.key.keysym.sym);
                     break;
 
                 /* Mouvement souris */
+                /*
                 case SDL_MOUSEMOTION:
                     printf("clic en (%d, %d)\n", e.motion.x, e.motion.y);
                     float r1, v1, b1;
@@ -90,9 +159,11 @@ int main(int argc, char** argv) {
                     b1 = 0;
                     glClearColor(r1, v1, b1, 1);
                     break;
+                */
 
+                /* Resize fenetre */
                 case SDL_VIDEORESIZE:
-                    dessin(e.resize.w, e.resize.h);
+                    resize(e.resize.w, e.resize.h);
                     break;
 
                 default:
