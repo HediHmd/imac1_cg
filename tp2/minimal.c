1619dcc609dc7e0b4172cdc6e91a7a5a801d02af
HAMADACHE HEDI <hhamadac@localhost.localdomain>
2018-02-26 08:34:53 +0100

    tp2 milieu

--- tp2/minimal.c
+++ tp2/minimal.c
@@ -92,13 +92,8 @@ Primitive* allocPrimitive(GLenum primitiveType) {
 void addPrimitive(Primitive* primitive, PrimitiveList* list) {
     assert(primitive);
     assert(list);
-    if (*list == NULL) {
-        // Si le pointeur sur la liste est vide, on le modifie pour le faire pointer sur la primitive donnée en paramètre
-        *list = primitive;
-    } else {
-        // Sinon, on réapplique la même fonction mais sur le point suivant
-        addPrimitive(primitive, &(*list)->next);
-    }
+    primitive->next = *list;
+    (*list) = primitive;
 }
 
 void drawPrimitives(PrimitiveList list) {
@@ -128,34 +123,6 @@ void resizeViewport() {
     SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_RESIZABLE);
 }
 
-
-static const unsigned char COLORS[] = {
-    255, 255, 255,
-    0, 0, 0,
-    255, 0, 0,
-    0, 255, 0,
-    0, 0, 255,
-    255, 255, 0,
-    0, 255, 255,
-    255, 0, 255
-};
-
-static const unsigned int NB_COLORS = sizeof(COLORS) / (3 * sizeof(unsigned char));
-
-void drawColorPalette() {
-    int i;
-    float dx = 2.f / NB_COLORS; // dx = "delta x"
-    glBegin(GL_QUADS);
-    for(i = 0; i < NB_COLORS; ++i) {
-        glColor3ubv(COLORS + i * 3);
-        glVertex2f(-1 + i * dx, -1);
-        glVertex2f(-1 + (i + 1) * dx, -1);
-        glVertex2f(-1 + (i + 1) * dx, 1);
-        glVertex2f(-1 + i  * dx, 1);
-    }
-    glEnd();
-}
-
 int main(int argc, char** argv) {
 
     /* Initialisation de la SDL */
@@ -169,21 +136,17 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Impossible d'ouvrir la fenetre. Fin du programme.\n");
         return EXIT_FAILURE;
     }
-    SDL_WM_SetCaption("Paint IMAC", NULL);
+
+    SDL_WM_SetCaption("TP2", NULL);
 
     glClearColor(0.1, 0.1, 0.1, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
 
     /* On créé une première primitive par défaut */
-    PrimitiveList primitives = NULL;
-    Primitive* prim = allocPrimitive(GL_POINTS);
-    PrimitiveList lastPrim = prim;
-    addPrimitive(prim, &primitives);
+    PrimitiveList primitives = allocPrimitive(GL_LINE_STRIP);
 
     int loop = 1;
-    int mode = 0; // le mode d'affichage. 0 = le dessin, 1 = la palette
-    unsigned int currentColor = 0; // l'index de la couleur courante dans le tableau COLORS
-    GLenum currentPrimitiveType = GL_POINTS;
+    float x, y;
 
     /* Boucle d'affichage */
     while(loop) {
@@ -192,22 +155,15 @@ int main(int argc, char** argv) {
         Uint32 startTime = SDL_GetTicks();
         
         /* Code de dessin */
-
         glClear(GL_COLOR_BUFFER_BIT); // Toujours commencer par clear le buffer
-
-        if (mode == 0) {
-            drawPrimitives(primitives); // On dessine la liste de primitives
-        }
-        else if (mode == 1) {
-            drawColorPalette();
-        }
+        drawPrimitives(primitives);
 
         /* Boucle traitant les evenements */
         SDL_Event e;
-        while(SDL_PollEvent(&e)) {
+        while(SDL_PollEvent(&e)){
 
             /* L'utilisateur ferme la fenêtre : */
-            if(e.type == SDL_QUIT) {
+            if(e.type == SDL_QUIT){
                 loop = 0;
                 break;
             }
@@ -215,83 +171,22 @@ int main(int argc, char** argv) {
             /* Quelques exemples de traitement d'evenements : */
             switch(e.type) {
 
-                /* Clic souris */
-                case SDL_MOUSEBUTTONUP:
-                    if (mode == 1) {
-                        /* On retrouve automatiquement l'index de la couleur cliquée en connaissant la taille de notre tableau */
-                        currentColor = e.button.x * NB_COLORS / WINDOW_WIDTH;
-                    }
-                    else if (mode == 0) {
-                        /* Transformation des coordonnées du clic souris en coordonnées OpenGL */
-                        float x = -1 + 2. * e.button.x / WINDOW_WIDTH;
-                        float y = -(-1 + 2. * e.button.y / WINDOW_HEIGHT);
-                        /* On retrouve la couleur courante grace à son index */
-                        unsigned int r = COLORS[currentColor * 3];
-                        unsigned int g = COLORS[currentColor * 3 + 1];
-                        unsigned int b = COLORS[currentColor * 3 + 2];
-                        /* On ajoute un nouveau point à la liste de la primitive courante */
-                        addPointToList(allocPoint(x, y, r, g, b), &lastPrim->points);
-                    }
-                    break;
-
                 /* Touche clavier */
                 case SDL_KEYDOWN:
-
                     printf("touche pressée (code = %d)\n", e.key.keysym.sym);
-
-                    if (e.key.keysym.sym == SDLK_SPACE) {
-                        mode = 1;
-                    }
-
-                    int newPrimitivePressed = 0;
-                    GLenum newPrimitiveType;
-
-                    switch(e.key.keysym.sym) {
-
-                        case SDLK_q:
-                            loop = 0;
-
-                        case SDLK_SPACE:
-                            mode = 1;
-                            break;
-
-                        case SDLK_p:
-                            newPrimitivePressed = 1;
-                            newPrimitiveType = GL_POINTS;
-                            break;
-
-                        case SDLK_l:
-                            newPrimitivePressed = 1;
-                            newPrimitiveType = GL_LINES;
-                            break;
-
-                        case SDLK_t:
-                            newPrimitivePressed = 1;
-                            newPrimitiveType = GL_TRIANGLES;
-                            break;
-                        
-                        case SDLK_c:
-                            /* Touche pour effacer le dessin */
-                            deletePrimitive(&primitives); // on supprime les primitives actuelles
-                            addPrimitive(allocPrimitive(currentPrimitiveType), &primitives); // on réinitialise à la primitive courante
-                            break;
-
-                        default:
-                            break;
-                    }
-
-                    if (newPrimitivePressed && currentPrimitiveType != newPrimitiveType) {
-                        prim = allocPrimitive(newPrimitiveType);
-                        lastPrim = prim;
-                        addPrimitive(prim, &primitives);
-                        currentPrimitiveType = newPrimitiveType;
-                    }
-
                     break;
 
-                case SDL_KEYUP:
-                    if (e.key.keysym.sym == SDLK_SPACE) {
-                        mode = 0;
+                case SDL_MOUSEBUTTONUP:
+                    /* Transformation des coordonnées du clic souris en coordonnées OpenGL */
+                    x = -1 + 2. * e.button.x / WINDOW_WIDTH;
+                    y = -(-1 + 2. * e.button.y / WINDOW_HEIGHT);
+                    /* On ajoute un nouveau point à la liste de la primitive courante */
+                    addPointToList(allocPoint(x, y, 255, 255, 255), &(primitives->points));
+                    if(e.button.button == SDL_BUTTON_RIGHT){
+                        primitives->primitiveType = GL_LINE_LOOP;
+                        //primitives->points = primitives->next->points;
+                        addPrimitive(allocPrimitive(GL_LINE_STRIP),&primitives);
+                    
                     }
                     break;
 
